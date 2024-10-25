9d6b24514858a6dfee6b3f43d3551fdb28dd0f50
Zay-Ch <zaynab.choudhury@ada.ac.uk>
2024-10-25 13:45:45 +0000

    validInteger

--- oop.js
+++ oop.js
@@ -1,9 +1,20 @@
 PRIORITY = { "LOW": 1, "MEDIUM": 3, "HIGH": 5, "URGENT": 7 };
 
 
-function validInteger (value) { // value can be a string or a number (integer)
-  
-}  
+function validInteger(value) {
+  const number = Number(value);
+  // Check if the conversion resulted in a valid number
+  if (isNaN(number)) {
+    return false;
+  }
+  // Check if the number is a positive integer
+  if (number > 0 && Number.isInteger(number)) {
+    console.log("The number can be represented as a valid integer.");
+    return true;
+  }
+  return false;
+}
+
 
 
 function validatePriority(priority) { // value can be a string or a number (integer)
